6eb683f69e7167ab8ff6a5aab275826f864a68f2
Emil Kuhejda <emil.kuhejda@gmail.com>
2025-06-12 19:33:27 +0200

    Clean up template files

---
+++ EduShop.Server/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduShop.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+
+            return Ok();
+        }
+    }
+}
