5b7eb23b22bbd30dcf7e95d1b57b21606cc39a97
Emil Kuhejda <emil.kuhejda@gmail.com>
2025-06-12 19:48:02 +0200

    Retrieve products from the web API

---
+++ EduShop.Client/Http/WebClient.cs
@@ -0,0 +1,23 @@
+﻿using EduShop.Shared.Contracts;
+using Newtonsoft.Json;
+
+namespace EduShop.Client.Http
+{
+    public interface IWebClient
+    {
+        Task<ProductDto[]> GetProductsAsync(CancellationToken cancellationToken);
+    }
+
+    internal class WebClient(IHttpClientFactory clientFactory) : IWebClient
+    {
+        public async Task<ProductDto[]> GetProductsAsync(CancellationToken cancellationToken)
+        {
+            var client = clientFactory.CreateClient(nameof(WebClient));
+            var response = await client.GetAsync("product", cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+            return JsonConvert.DeserializeObject<ProductDto[]>(content) ?? [];
+        }
+    }
+}
--- EduShop.Server/Controllers/ProductController.cs
+++ EduShop.Server/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduShop.Shared.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EduShop.Server.Controllers
 {
@@ -7,11 +8,31 @@ namespace EduShop.Server.Controllers
     public class ProductController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        [ProducesResponseType(typeof(ProductDto[]), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetAllAsync(CancellationToken cancellationToken)
         {
-            await Task.CompletedTask;
+            var products = new List<ProductDto>
+            {
+                new()
+                {
+                    Name = "Item 1",
+                    Description = "Description 1",
+                    Price = 10,
+                    Amount = 100,
+                    DateCreated = DateTime.Now
+                },
+                new()
+                {
+                    Name = "Item 2",
+                    Description = "Description 2",
+                    Price = 11,
+                    Amount = 110,
+                    DateCreated = DateTime.Now
+                }
+            };
 
-            return Ok();
+            return Ok(products);
         }
     }
 }
