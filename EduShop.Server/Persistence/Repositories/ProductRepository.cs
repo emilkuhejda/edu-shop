edac2b4c3c8605275a65685429e50f0b0dd610dc
Emil Kuhejda <emil.kuhejda@gmail.com>
2025-06-13 13:52:50 +0200

    Remove IRepository interface (#8)

--- EduShop.Server/Persistence/Repositories/ProductRepository.cs
+++ EduShop.Server/Persistence/Repositories/ProductRepository.cs
@@ -1,8 +1,46 @@
 ﻿using EduShop.Server.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduShop.Server.Persistence.Repositories
 {
-    public interface IProductRepository : IRepository<Product>;
+    public interface IProductRepository
+    {
+        Task AddAsync(Product product);
 
-    internal class ProductRepository(DatabaseContext context) : Repository<Product>(context), IProductRepository;
+        void Remove(Product product);
+
+        Task<Product?> GetAsync(Guid productId, CancellationToken cancellationToken);
+
+        Task<Product[]> GetAllAsync(CancellationToken cancellationToken);
+
+        Task SaveAsync(CancellationToken cancellationToken);
+    }
+
+    internal class ProductRepository(DatabaseContext context) : IProductRepository
+    {
+        public async Task AddAsync(Product product)
+        {
+            await context.Products.AddAsync(product);
+        }
+
+        public void Remove(Product product)
+        {
+            context.Products.Remove(product);
+        }
+
+        public Task<Product?> GetAsync(Guid productId, CancellationToken cancellationToken)
+        {
+            return context.Products.SingleOrDefaultAsync(x => x.Id == productId, cancellationToken);
+        }
+
+        public Task<Product[]> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return context.Products.ToArrayAsync(cancellationToken);
+        }
+
+        public Task SaveAsync(CancellationToken cancellationToken)
+        {
+            return context.SaveChangesAsync(cancellationToken);
+        }
+    }
 }
